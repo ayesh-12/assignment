038221a459b1e69e7decc56e0d6dec522519adfe
ayesh-12 <ayeshashamimayeshashamim@gmail.com>
2024-12-29 10:02:40 -0800

    Add files via upload

---
+++ main.j.s
@@ -0,0 +1,55 @@
+alert("Welcome to  JavaScript!");
+var name =prompt("what is your name");
+alert("Hello,"+name);
+var Firstname=prompt("Enter your Firstname");
+var lastname=prompt("Enter your lastname");
+var c=Firstname + lastname
+alert("your full name is "   +c);
+var favouritehobby="Garderning";
+alert("my favourite hobby is " + favouritehobby)
+
+var currentYear=2024;
+var yearofbirth = 2006;
+var age =2024 - 2006;
+alert("your age is " +age);
+
+var length = 10;
+var width = 5;
+var area = length * width;
+alert("The area of the rectangle is"  + area);
+
+var Ayesha ;
+var shamim;
+var class_one;
+
+var 123;
+var Ayesha-shamim;
+var .abc;
+ 
+
+
+var myage= 18;
+alert("my age is " + myage);
+
+var myage=19;
+alert("my friend's age is " + myage);
+
+
+var num1= prompt(" Enter the  first number");
+var num2= prompt(" Enter the  second number");
+num1 = 2;
+num2 = 5;
+var sum = num1 + num2;
+alert("the sum is "+ sum);
+
+
+var result = 6 * 8;
+alert("The result of 6 * 8 is" + result);
+
+var remainder = 17 % 5;
+alert("The remainder of 17 divided by 5 is " + remainder);
+
+
+
+
+ 
